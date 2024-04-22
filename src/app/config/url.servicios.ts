2cf214db6f1b8fb03c4f2540912a9414eaae918d
arod1967 <antoniojrodrigue@hotmail.com>
2024-04-22 08:11:22 -0500

    commit 20240422 v1

--- src/app/config/url.servicios.ts
+++ src/app/config/url.servicios.ts
@@ -1,11 +1,15 @@
 //Para PRODUCCION
-export const URL_SERVICIOS = "https://apibrands.estoespamii.com/api";
+//export const URL_SERVICIOS = "https://apibrands.estoespamii.com/api";
 
 //Para DESARROLLO
 //export const URL_SERVICIOS = "http://localhost:3030/api";
 
+
 //Para PRODUCCION
-export const URL_SERVICIOS_PAMII = "https://apibrands.estoespamii.com/api";
+//export const URL_SERVICIOS_PAMII = "https://apibrands.estoespamii.com/api";
+
+//Para DESARROLLO
+export const URL_SERVICIOS_PAMII = "http://localhost:3030/api";
 
 //export const URL_SERVICIOS_GEOCERCAS = "https://marketplace-project-f0cf3-default-rtdb.firebaseio.com";
 export const URL_SERVICIOS_GEOCERCAS = "https://marketplace-project-f0cf3-default-rtdb.firebaseio.com";
